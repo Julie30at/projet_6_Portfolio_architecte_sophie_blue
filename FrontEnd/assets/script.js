c57c5a46025587435bd656cda8f5c3d6c60f2760
Julie AT <julie30at@gmail.com>
2024-07-22 19:14:46 +0200

    logique ajout et supression de projet mise en place, css à travailler

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -12,10 +12,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const loginLink = document.getElementById('infoLog');
     const editLink = document.getElementById('editLink');
     const editBar = document.getElementById('editionMode');
-    const addNewProjectForm = document.getElementById('addNewProject');
-    const addPicture = document.querySelector('.addPicture');
+    const fileInput = document.getElementById('image');
     const previewImage = document.getElementById('previewImage');
-    const validateNewProject = document.getElementById('btnValidate');
+    const form = document.getElementById('addNewProject');
+    const titleInput = document.getElementById('title');
+    const categoryInput = document.getElementById('category');
+    const errorMessage = document.getElementById('error-message');
    
     // Initialisation de l'application
     init();
@@ -34,46 +36,30 @@ document.addEventListener('DOMContentLoaded', function() {
         fetchWorks();
         // Récupère et affiche les catégories
         fetchCategories();
-
-         // Supprime le token du localStorage à la fermeture de la page
-        window.addEventListener('beforeunload', () => {
-            localStorage.removeItem('token');
-        });
     }
 
     // Fonction asynchrone pour récupérer les projets de l'API
     async function fetchWorks(categoryId = null) {
         try {
-            // Appel API pour récupérer les projets
             const response = await fetch("http://localhost:5678/api/works");
-            // Conversion de la réponse en JSON
             const projects = await response.json();
-            // Mise à jour de la galerie avec les projets récupérés
             updateGallery(projects, categoryId);
         } catch (error) {
-            // Affichage d'un message d'erreur en cas d'échec de l'appel API
             console.error('Erreur lors de la récupération des projets:', error);
         }
     }
 
     // Fonction pour mettre à jour la galerie avec les projets
     function updateGallery(projects, categoryId) {
-        // Efface le contenu actuel de la galerie
         gallery.innerHTML = '';
-        // Récupération de l'élément galerie dans la modal
         const modalGallery = document.getElementById("modal-gallery");
-        // Efface le contenu actuel de la galerie de la modal
         modalGallery.innerHTML = '';
 
-        // Parcours chaque projet
         projects.forEach(work => {
-            // Filtre les projets par catégorie si nécessaire
             if (categoryId === null || work.categoryId === categoryId) {
-                // Crée une figure pour chaque projet et l'ajoute à la galerie
                 const figure = createFigure(work);
                 gallery.appendChild(figure);
 
-                // Crée une figure pour chaque projet et l'ajoute à la galerie de la modal
                 const modalFigure = createFigure(work, true);
                 modalFigure.classList.add("modal-figure");
                 modalGallery.appendChild(modalFigure);
@@ -81,126 +67,99 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-   // Fonction pour créer une figure pour un projet
-function createFigure(work, isModal = false) {
-    // Crée les éléments nécessaires (figure, img, figcaption)
-    const figure = document.createElement("figure");
-    const img = document.createElement("img");
-    const figcaption = document.createElement("figcaption");
+    // Fonction pour créer une figure pour un projet
+    function createFigure(work, isModal = false) {
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        const figcaption = document.createElement("figcaption");
 
-    // Défini les propriétés de l'image et du texte
-    img.src = work.imageUrl;
-    img.alt = work.title;
-    figcaption.textContent = work.title;
+        img.src = work.imageUrl;
+        img.alt = work.title;
+        figcaption.textContent = work.title;
 
-    // Ajoute l'image et la légende à la figure
-    figure.appendChild(img);
-    figure.appendChild(figcaption);
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
 
-    if (isModal) {
-        // Ajoute un bouton de suppression pour chaque figure dans la modal
-        const deleteButton = document.createElement("button");
-        deleteButton.innerHTML = '<i class="fa fa-trash"></i>'; //icône de suppression
-        deleteButton.classList.add("delete-button");
-        deleteButton.addEventListener('click', () => {
-            console.log(`Projet ${work.title} supprimé avec succès.`);
-            //deleteProject(work, figureElement);
-        });
-        figure.appendChild(deleteButton);
-    }
-
-    // Retourne la figure créée
-    return figure;
-}
-
-// Fonction pour supprimer un projet
-async function deleteProject(work, figureElement) {
-    // Récupère le token d'authentification depuis le localStorage
-    const token = localStorage.getItem('token');
-    if (!token) {
-        // Si aucun token n'est trouvé, alerte l'utilisateur qu'il doit être connecté
-        alert('Vous devez être connecté pour supprimer un projet.');
-        return;
-    }
-
-    try {
-        // Appel API pour supprimer le projet
-        const response = await fetch(`http://localhost:5678/api/works/${work.id}`, {
-            method: 'DELETE',
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        });
-
-        // Vérifie si la réponse de l'API est correcte
-        if (response.ok) {
-            // Si la réponse est OK (200), supprime la figure du DOM
-            figureElement.remove();
-            console.log(`Projet ${work.title} supprimé avec succès.`);
-        } else {
-            // Si la réponse n'est pas OK, affiche un message d'erreur
-            if (response.status === 401) {
-                alert('Vous n\'êtes pas autorisé à supprimer ce projet. Assurez-vous que vous êtes connecté.');
-            } else {
-                console.error('Erreur lors de la suppression du projet:', response.statusText);
-                alert('Erreur lors de la suppression du projet.');
-            }
+        if (isModal) {
+            const deleteButton = document.createElement("button");
+            deleteButton.innerHTML = '<i class="fa fa-trash"></i>';
+            deleteButton.classList.add("delete-button");
+            deleteButton.addEventListener('click', () => {
+                console.log(`Projet ${work.title} supprimé avec succès.`);
+                deleteProject(work, figure);
+            });
+            figure.appendChild(deleteButton);
         }
-    } catch (error) {
-        // En cas d'erreur avec l'appel API, affiche un message d'erreur
-        console.error('Erreur lors de la suppression du projet:', error);
-        alert('Erreur lors de la suppression du projet.');
+
+        return figure;
     }
-}
+
+    // Fonction pour supprimer un projet
+    async function deleteProject(work, figureElement) {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            alert('Vous devez être connecté pour supprimer un projet.');
+            return;
+        }
+
+        try {
+            const response = await fetch(`http://localhost:5678/api/works/${work.id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            if (response.ok) {
+                figureElement.remove();
+                console.log(`Projet ${work.title} supprimé avec succès.`);
+            } else {
+                if (response.status === 401) {
+                    alert('Vous n\'êtes pas autorisé à supprimer ce projet. Assurez-vous que vous êtes connecté.');
+                } else {
+                    console.error('Erreur lors de la suppression du projet:', response.statusText);
+                    alert('Erreur lors de la suppression du projet.');
+                }
+            }
+        } catch (error) {
+            console.error('Erreur lors de la suppression du projet:', error);
+            alert('Erreur lors de la suppression du projet.');
+        }
+    }
 
     // Fonction asynchrone pour récupérer les catégories des projets de l'API
     async function fetchCategories() {
         try {
-            // Appel API pour récupérer les catégories
             const response = await fetch("http://localhost:5678/api/categories");
-            // Conversion de la réponse en JSON
             const categories = await response.json();
-            // Mise à jour des boutons de catégorie avec les catégories récupérées
             updateCategoryButtons(categories);
         } catch (error) {
-            // Affichage d'un message d'erreur en cas d'échec de l'appel API
             console.error('Erreur lors de la récupération des catégories:', error);
         }
     }
 
     // Fonction pour mettre à jour les boutons de filtre avec les catégories
     function updateCategoryButtons(categories) {
-        // Efface le contenu actuel des boutons de filtre
         btnFilter.innerHTML = '';
 
-        // Crée un ensemble de catégories unique avec une catégorie "Tous" par défaut
-        const categorySet = new Set([{ id: null, name: "Tous" }, ...categories]);
+        const allCategories = [{ id: null, name: "Tous" }, ...categories];
         let selectedButton = null;
 
-        // Parcours chaque catégorie
-        categorySet.forEach(category => {
-            // Crée un bouton pour chaque catégorie
+        allCategories.forEach(category => {
             const buttonCtg = document.createElement("button");
             buttonCtg.id = category.id;
             buttonCtg.textContent = category.name;
             buttonCtg.classList.add("filter-button");
 
-            // Ajoute le bouton de catégorie au conteneur de filtres
             btnFilter.appendChild(buttonCtg);
 
-            // Ajoute un écouteur d'événements pour filtrer les projets par catégorie lorsque le bouton est cliqué
             buttonCtg.addEventListener("click", () => {
-                console.log(`Projets de la catégorie : ${category.name}`);
-
-                // Réinitialise les styles du bouton sélectionné précédemment
                 if (selectedButton) {
                     selectedButton.classList.remove('active');
                 }
-                // Met à jour le bouton sélectionné
                 selectedButton = buttonCtg;
                 selectedButton.classList.add('active');
 
-                // Récupère et affiche les projets de la catégorie sélectionnée
                 fetchWorks(category.id);
             });
         });
@@ -208,25 +167,94 @@ async function deleteProject(work, figureElement) {
 
     // Fonction pour configurer les écouteurs d'événements
     function setupEventListeners() {
-        // Affiche la modal principale lors du clic sur le lien d'édition
         editLink.addEventListener("click", showModal);
-        // Ferme la modal principale et affiche la deuxième modal lors du clic sur le bouton "addProject"
         addProject.addEventListener("click", () => {
             closeModal();
             showModal2();
         });
-        // Ferme la modal principale lors du clic sur le bouton de fermeture
+
+        fileInput.addEventListener('change', function(event) {
+            const file = event.target.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    previewImage.src = e.target.result;
+                };
+                reader.readAsDataURL(file);
+                console.log("Aperçu photo");
+            }
+        });
+
+        form.addEventListener('submit', async function(event) {
+            event.preventDefault();
+
+            if (validateForm()) {
+                // Conversion de la catégorie en ID numérique
+                const categoryName = categoryInput.value;
+                const categoryId = getCategoryId(categoryName);
+
+                if (categoryId === null) {
+                    errorMessage.textContent = 'Catégorie non reconnue.';
+                    return;
+                }
+
+                // Création du FormData pour l'envoi
+                const formData = new FormData();
+                formData.append('image', fileInput.files[0]);
+                formData.append('title', titleInput.value);
+                formData.append('category', categoryId);
+
+                // Récupération du token d'authentification
+                const token = localStorage.getItem('token');
+                if (!token) {
+                    alert('Vous devez être connecté pour ajouter un projet.');
+                    return;
+                }
+
+                // Envoi de la requête POST
+                try {
+                    const response = await fetch("http://localhost:5678/api/works", {
+                        method: 'POST',
+                        headers: {
+                            'Authorization': `Bearer ${token}`,
+                            'Accept': 'application/json'
+                        },
+                        body: formData
+                    });
+
+                    if (response.ok) {
+                        const data = await response.json();
+                        console.log('Projet ajouté:', data);
+                        form.reset();
+                        previewImage.src = './assets/icons/picture-svgrepo-com.svg'; 
+                        closeModal2(); 
+                        fetchWorks(); 
+                    } else if (response.status === 400) {
+                        errorMessage.textContent = 'Les données envoyées sont incorrectes. Veuillez vérifier les champs.';
+                    } else if (response.status === 401) {
+                        alert('Non autorisé. Veuillez vous connecter.');
+                    } else if (response.status === 500) {
+                        alert('Erreur interne. Veuillez réessayer plus tard.');
+                    } else {
+                        alert('Erreur lors de l\'envoi des données.');
+                    }
+                } catch (error) {
+                    console.error('Erreur réseau:', error);
+                    alert('Erreur de réseau. Veuillez réessayer plus tard.');
+                }
+            } else {
+                errorMessage.textContent = 'Veuillez remplir tous les champs correctement.';
+            }
+        });
+        
         closeBtn.addEventListener("click", closeModal);
-        // Ferme la deuxième modal lors du clic sur le bouton de fermeture
         closeBtn2.addEventListener("click", closeModal2);
-        // Ferme la deuxième modal et affiche la modal principale lors du clic sur la flèche de retour
         backArrow.addEventListener("click", () => {
             closeModal2();
             showModal();
-            console.log("retour modal");
+            console.log("Retour modal");
         });
 
-        // Ferme la modal principale lors du clic en dehors du contenu de la modal
         modal.addEventListener('click', (event) => {
             if (event.target === modal) {
                 closeModal();
@@ -234,7 +262,6 @@ async function deleteProject(work, figureElement) {
             }
         });
 
-        // Ferme la deuxième modal lors du clic en dehors du contenu de la modal
         modal2.addEventListener('click', (event) => {
             if (event.target === modal2) {
                 closeModal2();
@@ -242,7 +269,6 @@ async function deleteProject(work, figureElement) {
             }
         });
 
-        // Ferme les modales lors de l'appui sur la touche Échap
         document.addEventListener('keydown', (event) => {
             if (event.key === 'Escape') {
                 closeModal();
@@ -271,6 +297,33 @@ async function deleteProject(work, figureElement) {
         console.log("Afficher modal2");
     }
 
+    // Fonction pour valider le formulaire
+    function validateForm() {
+        const file = fileInput.files[0];
+        const title = titleInput.value;
+        const category = categoryInput.value;
+
+        console.log("Validation Form: ", {
+        file: file,
+        title: title,
+        category: category
+    });
+
+        if (title.trim() !== '' && category.trim() !== '' && file) {
+            return true;
+        } else {
+            errorMessage.classList.remove('hidden');
+            if (title.trim() === '') {
+                errorMessage.textContent = 'Le titre est requis.';
+            } else if (category.trim() === '') {
+                errorMessage.textContent = 'La catégorie est requise.';
+            } else if (!file) {
+                errorMessage.textContent = 'Une image est requise.';
+            }
+            return false;
+        }
+    }
+
     // Fonction pour fermer la deuxième modal
     function closeModal2() {
         modal2.style.display = 'none';
@@ -279,30 +332,31 @@ async function deleteProject(work, figureElement) {
 
     // Fonction pour mettre à jour les liens de connexion et d'édition
     function updateLinks() {
-        // Vérifie si un token est présent dans le localStorage
         if (localStorage.getItem('token')) {
-            // Change le texte du lien de connexion en "logout"
             loginLink.textContent = 'logout';
-            // Ajoute un écouteur d'événements pour gérer la déconnexion
             loginLink.addEventListener('click', function(event) {
                 event.preventDefault();
-                // Supprime le token du localStorage
                 localStorage.removeItem('token');
-                // Redirige vers la page d'accueil
                 window.location.href = 'index.html';
             });
 
-            // Affiche les éléments d'édition si l'utilisateur est connecté
-            editLink.style.display = 'visible';
-            editBar.style.display = 'visible';
-            // Masque les boutons de filtre
+            editLink.style.display = 'inline';
+            editBar.style.display = 'block';
             btnFilter.style.display = 'none';
         } else {
-            // Change le texte du lien de connexion en "login"
             loginLink.textContent = 'login';
-            // Masque les éléments d'édition si l'utilisateur n'est pas connecté
             editLink.style.display = 'none';
             editBar.style.display = 'none';
         }
+    }
+
+    // Fonction pour obtenir l'ID de la catégorie à partir du nom
+    function getCategoryId(categoryName) {
+        const categories = {
+            "Objets": 1,
+            "Appartements": 2,
+            "Hotels & restaurants": 3
+        };
+        return categories[categoryName] || null;
     }
 });
